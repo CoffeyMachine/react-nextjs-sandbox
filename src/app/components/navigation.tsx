f059a9f75d9998edaa84f8dd513eaf7569e0c58b
David Coffey <david@coffeymachine.com>
2023-12-19 15:34:13 -0700

    Implement favorites counter

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,10 +1,19 @@
+import { useAppSelector } from '@/lib/hooks'
 import Link from 'next/link'
 
 const Navigation = () => {
+  const state = useAppSelector((state) => state)
+  const { favorites } = state
+
   return (
     <div className='p-3 m-3 flex justify-center'>
       <Link href="/" className='border-solid border border-gray-500 rounded-md p-2 mr-3'>Upcoming Events</Link>
-      <Link href="/favorites" className='border-solid border border-gray-500 rounded-md p-2'>Favorite Events</Link>
+      <Link href="/favorites" className='border-solid border border-gray-500 rounded-md p-2'>
+        Favorite Events
+        {favorites.favoriteEvents.length? 
+          <span className='m-3 px-3 py-1 rounded-full bg-sky-500'>{favorites.favoriteEvents.length}</span>
+         : ''}
+      </Link>
     </div>
   )
 }
