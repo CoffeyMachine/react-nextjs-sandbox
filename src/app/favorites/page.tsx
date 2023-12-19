9626d5c28f204178568f426becc2f11fbf36e757
David Coffey <david@coffeymachine.com>
2023-12-19 15:15:36 -0700

    Implement Clear All Favorites button

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 // import Image from 'next/image'
-import { useAppSelector } from '@/lib/hooks'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import EventListing from '../components/eventListing'
 import Navigation from '../components/navigation'
-
+import favoritesSlice from '@/lib/favoritesSlice'
+import Link from 'next/link'
 
 export default function Home() {
   const state = useAppSelector((state) => state)
+  const dispatch = useAppDispatch();
   const { favorites } = state
+  const { clearAllFavorites } = favoritesSlice.actions
 
   return (
     <div>
@@ -19,6 +22,16 @@ export default function Home() {
           </div>
         )
       })}
+      <div className='p-3 m-3 flex justify-center'>
+        {!favorites.favoriteEvents.length ? (
+          <p>You haven't added any favorites yet. Go to the <Link className="underline underline-offset-2" href="/">Upcoming Events</Link> page and add some!</p>
+        ) :
+        (
+          <button className="border-solid border border-gray-500 rounded-md p-2" onClick={() => dispatch(clearAllFavorites())}>
+            Clear all favorites
+          </button>
+        )}
+      </div>
     </div>
   )
 }
